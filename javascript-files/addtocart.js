2427c20aae3d9fc3d7350482b5752b06b2ccaabf
Rajeswari N <rajeswarinenavath2244@gmail.com>
2024-10-04 11:44:03 +0530

    final updated code

--- javascript-files/addtocart.js
+++ javascript-files/addtocart.js
@@ -1,41 +1,3 @@
-// let cart = {};
-// if (localStorage.getItem("cart")) {
-//     cart = JSON.parse(localStorage.getItem("cart"));
-// } else {
-//     alert("Cart is empty");
-
-//     text = "Cart is empty. ";
-//     document.getElementById("table-container").innerHTML = text;
-
-//     let goBack = document.createElement("a");
-//     goBack.href = "index.html";
-//     goBack.textContent = "Go back";
-//     document.getElementById("table-container").appendChild(goBack);
-// }
-
-// let tbody = document.getElementById("tbody");
-
-// for (let id in cart) {
-//     let item = cart[id];
-
-//     let tr = document.createElement('tr')
-
-//     let title_td = document.createElement('td')
-//     title_td.textContent = item.title
-//     tr.appendChild(title_td)
-
-
-//     let price_td = document.createElement("td");
-//     price_td.textContent = item.price;
-//     tr.appendChild(price_td);
-
-//     let qty_td = document.createElement("td");
-//     qty_td.textContent = item.qty;
-//     tr.appendChild(qty_td);
-
-//     tbody.appendChild(tr)
-
-// }
 
 
 
@@ -46,9 +8,7 @@ if(localStorage.getItem("cart")){
 
 }
 else{
-  // alert("cart is empty")
-  // text="Cart Is Empty"
-  // document.getElementById("empty").innerHTML=text
+  
   let variable=document.getElementById('empty')
   variable.innerHTML=`
   <div class="fs-3 d-flex justify-content-center align-items-center fw-bold">Cart</div>
